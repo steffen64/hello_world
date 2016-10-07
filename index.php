144e8c1da4182fba4d34330fb7f1134f76d7cb76
sp0001 <sp0001@users.noreply.github.com>
2016-10-07 09:18:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         $name = "Sascha";
-         echo "Meine Name ist $name";
+         echo 'Meine Name ist'. $name;
         
         ?>
     </body>
